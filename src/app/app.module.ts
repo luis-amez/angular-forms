75973d7620a43824290af7ef3d244606aff912c9
Luis Amez <luis.amez.vitoria@gmail.com>
2017-09-30 18:17:21 +0200

    Add form-sku component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormSkuComponent } from './form-sku/form-sku.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormSkuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
